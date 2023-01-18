4bc7d3e0e406f155254bfbe6bdf86ee4f3e54307
KamienKaze <HygrekRazuuk@gmail.com>
2023-01-18 18:30:32 +0100

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Egzamin_App';
+  courses : string[] = ["Programowanie w C#", "Angular dla początkujących", "Kurs Django"];
+
+  form = new FormGroup({
+    name : new FormControl(''),
+    number : new FormControl('')
+  });
+
+  onSubmit() {
+    const name = this.form.get("name")?.value;
+    const number = parseInt(<string>this.form.get("number")?.value) - 1;
+
+
+    console.log(name);
+
+    if(number < this.courses.length) {
+      console.log(this.courses[number])
+    } else {
+      console.log("Nieprawidłowy numer kursu")
+    }
+  }
 }
